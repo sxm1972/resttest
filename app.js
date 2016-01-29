2e9a40d4ed5727c65d9fcf4f5cd437573988f6cf
Sundeep Mallya <sxm1972@yahoo.com>
2016-01-29 09:51:56 +0000

    Added gulp support

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var x = 2;
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req,res){
-    res.send('Welcome to my API');
+    res.send('Welcome to my API!');
 });
 
 app.listen(port,process.env.IP,1,function() {
