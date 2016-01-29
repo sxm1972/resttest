c551546adb18a0317ac3e08e8e3aa2f911e5e084
Sundeep Mallya <sxm1972@yahoo.com>
2016-01-29 11:18:15 +0000

    Adding more files

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var Book = require('./models/bookmodel');
+
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req,res){
-    res.send('Welcome to my API!');
+    res.send('Welcome to resttest API!');
 });
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+    .get(function(req,res){
+        res.status(404).send('GET Not implemented');
+    })
+    .post(function(req,res){
+        var book = new Book(req.body);
+        book.save();
+        res.status(201).send(book);
+    })
+    .put(function(req,res){
+        res.status(404).send('PUT Not implemented');
+    })
+    .delete(function(req,res){
+        res.status(404).send('DELETE Not implemented');
+    })
+    .patch(function(req,res){
+        res.status(404).send(' PATCH Not implemented');
+    });
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json);
+app.use('/api',bookRouter);
 
 app.listen(port,process.env.IP,1,function() {
     console.log('Listening on ' + process.env.IP +':' + port);
