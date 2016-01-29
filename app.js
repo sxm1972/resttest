f797680d634869fe3df045d86e6cf2ace2fdd28f
Sundeep Mallya <sxm1972@yahoo.com>
2016-01-29 09:10:41 +0000

    First commit of my basic RESTful web-service

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var x = 2;
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req,res){
+    res.send('Welcome to my API');
+});
+
+app.listen(port,process.env.IP,1,function() {
+    console.log('Listening on ' + process.env.IP +':' + port);
+});
+
